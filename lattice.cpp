7b6f4541e72037749e12fed053cfe47a0738e2ea
shubham-bari <shubham.bari001@gmail.com>
2025-07-08 22:27:33 +0530

    -m'complete'

--- lattice.cpp
+++ lattice.cpp
@@ -1,15 +1,51 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 using namespace std;
 
-double callPrice(int S, int K, double sigma, double r, int T, int N)
+double biPrice(double S, double K, double sigma, double r, int T, int N, bool call)
 {
-    return 1.1f;
+    double dt = T/N;
+    double u= std::exp(sigma* (std::sqrt(dt)));
+    double d= 1/u;
+    double p = (std::exp(r* dt)-d)/(u-d);
+    double discount = std::exp(r* -dt);
+    
+    vector<double> assetp(N+1);
+
+    for(int i=0;i<N+1;i++)
+    {
+        assetp[i]=S*(std::pow(u, N-i))*(std::pow(d,i));
+    }
+
+    //forward prop
+    vector<double> optionval(N+1);
+    for(int i=0;i<=N;i++)
+    {
+        if(call)
+        {
+            optionval[i] = std::max(assetp[i]-K, 0.0);
+        }
+        else{
+            optionval[i] = std::max(K-assetp[i], 0.0);
+        }
+    }
+
+    //backward prop
+     for(int step=N-1;step>=0;step--)
+     {
+        for(int i=0;i<=step;i++)
+        {
+            optionval[i]= ((p*optionval[i]) + ((1-p)*optionval[i+1]))*discount;
+        }
+     }
+
+    return optionval[0];
 }
 
 int main()
 {
-    int S; int K; double sigma; double r; int T; int N; 
+    double S; double K; double sigma; double r; double T; double N; 
     cout<<"Enter the Stock price, Strike price, risk free rate,volatility, Maturity, Number of steps: "<<endl;
     cin>>S;
     cin>>K;
@@ -18,7 +54,9 @@ int main()
     cin>>T;
     cin>>N;
 
-    double call = callPrice(S, K, sigma, r, T, N);
-    cout<<call<<endl;
+    double call1 = biPrice(S, K, sigma, r, T, N, true);
+    double put = biPrice(S, K, sigma, r, T, N, false);
+
+    cout<<call1<<endl;
     return 0;
 }
